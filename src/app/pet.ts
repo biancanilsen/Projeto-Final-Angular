1c5ea1a25db22424542b330d61aa20449453d647
Lucas Cardoso <lucasfinho32@gmail.com>
2021-09-19 14:24:23 -0300

    19.09

--- src/app/pet.ts
+++ src/app/pet.ts
@@ -6,7 +6,8 @@ export class Pet {
     Name: string = ""; // nome do pet
     Animal: string = ""; // tipo do animal
     Breed: string = ""; // raça do animal
-    Age: number = 0; // idade
+    Age_years: number = 0; // idade
+    Age_months: number = 0;
     Size: string = ""; // porte, pequeno meido grande
     Adopted: boolean = false; 
     Old_owner_id: string = "";
