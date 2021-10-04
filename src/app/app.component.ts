7592911c2b237700d771ecf66a9505202044ede5
Lucas Edgar Cardoso <lucasfinho32@gmail.com>
2021-10-04 11:34:56 -0300

    20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,6 @@ export class AppComponent {
   constructor(private userServise: UserService, private petServise: PetService) {}
   
   ngOnInit(): void{
-    
+    this.userServise.getAllUsers
   }
 }
