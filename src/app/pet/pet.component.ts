10c134c9f3b2ad7d540b4e4d5d6bd16980b11d38
RamboRthr <arthurbrachtrambo@gmail.com>
2021-10-05 20:33:17 -0300

    20_33

--- src/app/pet/pet.component.ts
+++ src/app/pet/pet.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PhotoUploadComponent } from '../photo-upload/photo-upload.component';
 import { PetService } from '../services/pet.service';
+import { PhotoUploadService } from '../services/photo-upload.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -10,28 +12,44 @@ import { UserService } from '../services/user.service';
 })
 export class PetComponent implements OnInit {
 
-  constructor(public petService: PetService, public userService: UserService, private routerService: Router) { }
+  constructor(public petService: PetService, public userService: UserService, private routerService: Router,
+    private photoUploadService: PhotoUploadService) { }
 
   ngOnInit(): void {
     this.userService.getLogged();
   }
-  onSubmit(form: any){
+  onSubmit(form: any) {
     this.petService.formData.current_owner_id = this.userService.logged.Id
     this.postPet();
     this.routerService.navigateByUrl('/pet-confirm')
-    
+
   }
-    update(){
-      this.petService.updatePet(this.petService.formData);
-      
-    }
+  update() {
+    this.petService.updatePet(this.petService.formData);
+
+  }
+
+  getPet() {
+    this.petService.getPet(this.petService.formData.id);
+  }
+  postPet() {
+    this.petService.postPet();
+  }
+
+  fileToUpload: any = null;
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
+  uploadFileToActivity() {
+    this.photoUploadService.postFile(this.fileToUpload).subscribe(data => {
+      // do something, if upload success
+      }, error => {
+        console.log(error);
+      });
+  }
+
   
-    getPet(){
-      this.petService.getPet(this.petService.formData.id);
-    }
-    postPet(){
-      this.petService.postPet();
-    }
-    
-  
+}
 }
