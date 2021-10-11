2b1c97e9f1145926f776a75134ca2aadcb9b0aaa
Lucas Edgar Cardoso <lucasfinho32@gmail.com>
2021-10-11 08:25:15 -0300

    EXCLUSAO_PetConfim_Lucas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,6 @@ import { PhotoUploadComponent } from './photo-upload/photo-upload.component';
 import { PetPageComponent } from './pet-page/pet-page.component';
 import { AdoptPageComponent } from './adopt-page/adopt-page.component';
 import { ReportPageComponent } from './report-page/report-page.component';
-import { PetconfirmComponent } from './petconfirm/petconfirm.component';
-
-
 
 @NgModule({
   declarations: [
@@ -39,7 +36,6 @@ import { PetconfirmComponent } from './petconfirm/petconfirm.component';
     PetPageComponent,
     AdoptPageComponent,
     ReportPageComponent,
-    PetconfirmComponent,
     
   ],
   imports: [
