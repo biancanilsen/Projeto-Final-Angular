4b398996daa1e1420b34b5bceae0f4dc0ef8a057
RamboRthr <arthurbrachtrambo@gmail.com>
2021-10-11 20:11:04 -0300

    pet-menu pronto (menos css)

--- src/app/pet-menu/pet-menu.component.ts
+++ src/app/pet-menu/pet-menu.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Pet } from '../domain/pet';
 import { PetService } from '../services/pet.service';
 import { UserService } from '../services/user.service';
-
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 @Component({
   selector: 'app-pet-menu',
   templateUrl: './pet-menu.component.html',
@@ -9,11 +10,33 @@ import { UserService } from '../services/user.service';
 })
 export class PetMenuComponent implements OnInit {
 
-  constructor(public petService: PetService, public userService: UserService) { }
+  constructor(public petService: PetService, public userService: UserService,
+    private modalService: BsModalService) { }
+
+  modalRef?: BsModalRef;
+  message?: string;
 
   ngOnInit(): void {
     this.petService.getAllPets();
-    this.userService.getLogged
+    this.userService.getLogged();
+  }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
   }
 
+  confirm(pet: Pet): void {
+    this.deletePet(pet)
+    this.modalRef?.hide();
+    this.refresh()
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
+  }
+  deletePet(pet: Pet) {
+    this.petService.deletePet(pet)
+  }
+  refresh(): void {
+    window.location.reload();
+}
 }
